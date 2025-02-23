21b0944aaafd5dbbc31339e50fa31ab4fc5c6f96
Maurits van der Schee <maurits@vdschee.nl>
2025-02-23 17:00:58 +0100

    i18n third file

---
+++ i18n_convert.php
@@ -0,0 +1,47 @@
+<?php
+
+use PhpParser\Node;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\ParserFactory;
+
+if (!file_exists('vendor/autoload.php') || !file_exists('pages')) {
+    echo "Please run this script from the project root.\n";
+    exit(1);
+}
+
+// Load the libraries
+require 'vendor/autoload.php';
+
+$files = glob('i18n/*.po');
+foreach ($files as $file) {
+    $read = fopen($file, "r");
+    if (!$read) {
+        die("could not read po file: $file");
+    }
+    $strings = [];
+    $scanid = true;
+    $msgid = [];
+    $msgstr = [];
+    while (($line = fgets($read)) !== false) {
+        if (substr($line, 0, 5) == 'msgid') {
+            $msgid = [json_decode(substr(rtrim($line, "\n"), 6))];
+            $scanid = true;
+        } elseif (substr($line, 0, 6) == 'msgstr') {
+            $msgstr = [json_decode(substr(rtrim($line, "\n"), 7))];
+            $scanid = false;
+        } elseif (substr($line, 0, 1) == '"') {
+            if ($scanid) {
+                $msgid[] = json_decode(rtrim($line, "\n"));
+            } else {
+                $msgstr[] = json_decode(rtrim($line, "\n"));
+            }
+        } else {
+            $strings[implode('', $msgid)] = implode('', $msgstr);
+        }
+    }
+    $strings[implode('', $msgid)] = implode('', $msgstr);
+    unset($strings['']);
+    fclose($read);
+    file_put_contents(str_replace('.po', '.json', $file), json_encode($strings, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+}
